0aa6a3fa73457f4ad8ef9f2776a83818a71c969e
maxr96 <S1a5l6i3v2a>
2020-02-18 17:31:28 +0100

    Changed tests in order to make them pass both with Github CI and Bamboo

--- src/test/java/de/tum/in/www1/artemis/ManagementResourceIntegrationTest.java
+++ src/test/java/de/tum/in/www1/artemis/ManagementResourceIntegrationTest.java
@@ -1,26 +1,29 @@
 package de.tum.in.www1.artemis;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import de.tum.in.www1.artemis.config.audit.AuditEventConverter;
 import de.tum.in.www1.artemis.domain.PersistentAuditEvent;
-import de.tum.in.www1.artemis.repository.PersistenceAuditEventRepository;
 import de.tum.in.www1.artemis.service.AuditEventService;
 import de.tum.in.www1.artemis.service.feature.Feature;
 import de.tum.in.www1.artemis.util.DatabaseUtilService;
@@ -34,45 +37,43 @@ public class ManagementResourceIntegrationTest extends AbstractSpringIntegration
     @Autowired
     RequestUtilService request;
 
-    @Autowired
-    AuditEventService auditEventService;
-
     @SpyBean
-    PersistenceAuditEventRepository persistenceAuditEventRepository;
+    AuditEventService auditEventService;
 
     @Autowired
     AuditEventConverter auditEventConverter;
 
-    private PersistentAuditEvent persAuditEvent;
-
-    private List<PersistentAuditEvent> persEvents;
+    private List<PersistentAuditEvent> persAuditEvents;
 
     @BeforeEach
     public void initTestCase() {
         database.addUsers(2, 0, 0);
-        persEvents = persistenceAuditEventRepository.findAll();
-        persistenceAuditEventRepository.deleteAll();
-        persAuditEvent = new PersistentAuditEvent();
+        var persAuditEvent = new PersistentAuditEvent();
         persAuditEvent.setPrincipal("student1");
         persAuditEvent.setAuditEventDate(Instant.now());
         persAuditEvent.setAuditEventType("type");
         var data = new HashMap<String, String>();
         data.put("1", "2");
+        persAuditEvent.setId(1L);
         persAuditEvent.setData(data);
-        persAuditEvent = persistenceAuditEventRepository.save(persAuditEvent);
 
         var persAuditEvent2 = new PersistentAuditEvent();
         persAuditEvent2.setPrincipal("student2");
         persAuditEvent2.setAuditEventDate(Instant.now().minus(5, ChronoUnit.DAYS));
         persAuditEvent2.setAuditEventType("tt");
         persAuditEvent2.setData(data);
-        persistenceAuditEventRepository.save(persAuditEvent2);
+        persAuditEvents = new ArrayList<>() {
+
+            {
+                add(persAuditEvent);
+                add(persAuditEvent2);
+            }
+        };
     }
 
     @AfterEach
     public void tearDown() {
         database.resetDatabase();
-        persistenceAuditEventRepository.saveAll(persEvents);
     }
 
     @Test
@@ -88,6 +89,8 @@ public class ManagementResourceIntegrationTest extends AbstractSpringIntegration
     @Test
     @WithMockUser(username = "admin", roles = "ADMIN")
     public void getAllAuditEvents() throws Exception {
+        var persEvents = new PageImpl<>(persAuditEvents);
+        doReturn(persEvents).when(auditEventService).findAll(any());
         var auditEvents = request.getList("/management/audits", HttpStatus.OK, PersistentAuditEvent.class);
         assertThat(auditEvents.size()).isEqualTo(2);
     }
@@ -97,20 +100,24 @@ public class ManagementResourceIntegrationTest extends AbstractSpringIntegration
     public void getAllAuditEventsByDate() throws Exception {
         String pastDate = LocalDate.now().minusDays(2).toString();
         String currentDate = LocalDate.now().toString();
+        var persEvents = new PageImpl<>(new ArrayList<PersistentAuditEvent>() {
+
+            {
+                add(persAuditEvents.get(0));
+            }
+        });
+        doReturn(persEvents).when(auditEventService).findByDates(any(), any(), any());
         var auditEvents = request.getList("/management/audits?fromDate=" + pastDate + "&toDate=" + currentDate, HttpStatus.OK, PersistentAuditEvent.class);
         assertThat(auditEvents.size()).isEqualTo(1);
-        var auditEvent = auditEvents.get(0);
-        var auditEventsInDb = persistenceAuditEventRepository.findAllByAuditEventDateBetween(Instant.now().minus(2, ChronoUnit.DAYS), Instant.now(), Pageable.unpaged());
-        assertThat(auditEventsInDb.getTotalElements()).isEqualTo(1);
-        assertThat(auditEvent.getPrincipal()).isEqualTo(auditEventsInDb.get().findFirst().get().getPrincipal());
+        assertThat(persAuditEvents.get(0).getPrincipal()).isEqualTo(auditEvents.get(0).getPrincipal());
     }
 
     @Test
     @WithMockUser(username = "admin", roles = "ADMIN")
     public void getAuditEvent() throws Exception {
-        var auditEvent = request.get("/management/audits/" + persAuditEvent.getId(), HttpStatus.OK, PersistentAuditEvent.class);
+        doReturn(Optional.of(persAuditEvents.get(0))).when(auditEventService).find(persAuditEvents.get(0).getId());
+        var auditEvent = request.get("/management/audits/" + persAuditEvents.get(0).getId(), HttpStatus.OK, PersistentAuditEvent.class);
         assertThat(auditEvent).isNotNull();
-        var auditEventInDb = persistenceAuditEventRepository.findById(persAuditEvent.getId()).get();
-        assertThat(auditEventInDb.getPrincipal()).isEqualTo(auditEvent.getPrincipal());
+        assertThat(auditEvent.getPrincipal()).isEqualTo(persAuditEvents.get(0).getPrincipal());
     }
 }
